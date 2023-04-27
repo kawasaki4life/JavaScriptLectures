be09b45228ec18218c851b4d143a1561904cc206
unknown <melmarx777@gmail.com>
2023-04-27 15:34:05 +0800

    Initial Commit

---
+++ 01-variable-datatypes/02-comments-shortcuts/script.js
@@ -0,0 +1,11 @@
+/* Single line of codecomment */
+
+/* The shortcut key is: ctrl+ */
+
+// Multi line Comment: SHift + Alt + A
+
+console.log(100);
+console.log("Hello World");
+console.log(20, "Hello", true);
+    
+}
